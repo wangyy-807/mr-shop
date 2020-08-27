8a0eaae67d7d829a6ac84f5ed39487988599f8f9
e <live_in_solitude@163.com>
2020-08-27 19:54:39 +0800

    common服务搭建-api服务搭建-service服务搭建

---
+++ mingrui-shop-parent/mingrui-shop-service-api/mingrui-shop-service-api-xxx/src/main/java/com/baidu/shop/service/CategoryService.java
@@ -0,0 +1,25 @@
+package com.baidu.shop.service;
+
+import com.baidu.shop.base.Result;
+import com.baidu.shop.entities.CategoryEntity;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+/**
+ * @ClassName CategoryService
+ * @Description: TODO
+ * @Author wangyue
+ * @Date 2020/8/27
+ * @Version V1.0
+ **/
+@Api(tags = "商品分类接口")
+public interface CategoryService {
+
+    @ApiOperation(value = "通过查询商品分类")
+    @GetMapping(value = "category/list")
+    public Result<List<CategoryEntity>> getCategoryByPid(Integer pid);
+
+}
---
+++ mingrui-shop-parent/mingrui-shop-service/mingrui-shop-service-xxx/src/main/java/com/baidu/shop/mapper/CategoryMapper.java
@@ -0,0 +1,14 @@
+package com.baidu.shop.mapper;
+
+import com.baidu.shop.entities.CategoryEntity;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @ClassName CategoryMapper
+ * @Description: TODO
+ * @Author wangyue
+ * @Date 2020/8/27
+ * @Version V1.0
+ **/
+public interface CategoryMapper extends Mapper<CategoryEntity> {
+}
